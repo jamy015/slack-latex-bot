4a9a31325fb2c23ccd69fc07e238d852f29de4e2
Jamy Mahabier <jamysanjay@gmail.com>
2017-06-02 23:11:31 +0200

    Add bot

---
+++ bot.py
@@ -0,0 +1,42 @@
+import latex
+import os
+from flask import Flask, request, abort, jsonify
+
+app = Flask(__name__)
+
+slack_verification_token = os.environ["SLACK_VERIFICATION_TOKEN"]
+
+
+@app.route('/slack/latex', methods=['POST'])
+def slash_latex():
+    """Handle an incoming LaTeX slash command from Slack"""
+
+    if request.form['token'] != slack_verification_token:
+        abort(401)  # Unauthorized
+
+    try:
+        image = latex.to_image_url(request.form['text'])
+    except ValueError as err:
+        return jsonify({
+            'response_type': 'ephemeral',
+            'attachments': [{
+                'fallback': '*LaTeX Error:* {}'.format(err),
+                'color': 'danger',
+                'fields': [{
+                    'title': '*LaTeX Error*',
+                    'value': str(err),
+                }],
+            }],
+        })
+
+    return jsonify({
+        'response_type': 'in_channel',
+        'attachments': [{
+            'fallback': 'LaTeX',
+            'image_url': image,
+        }],
+    })
+
+
+if __name__ == "__main__":
+    app.run()
