9f8ae345375bd2b478cf4866608c21d65f7df2db
Jamy Mahabier <jamysanjay@gmail.com>
2017-07-20 21:04:35 +0200

    Correctly encode POST data
    Resolves #4.

--- quicklatex.py
+++ quicklatex.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlencode, quote
 import requests
 
 with open('default_preamble.txt') as default_preamble_file:
@@ -6,13 +7,15 @@ with open('default_preamble.txt') as default_preamble_file:
 
 def quicklatex(formula, preamble=default_preamble, show_errors=False):
     """Given a LaTeX formula, return an image URL"""
-    r = requests.post('http://quicklatex.com/latex3.f', data={
+    r = requests.post('http://quicklatex.com/latex3.f', data=urlencode({
         'formula': formula,
         'preamble': preamble,
         'fsize': '19px',
         'out': '1',  # output PNG
         'errors': '1' if show_errors else '0',
-    })
+    }, quote_via=quote))  # quote_via=quote ensures that any spaces won't get encoded as + signs
+                          # see https://docs.python.org/3.6/library/urllib.parse.html#urllib.parse.urlencode
+                          # and https://github.com/jamy015/slack-latex-bot/issues/4
 
     if r.status_code != 200:
         raise HTTPError(r.status_code)
