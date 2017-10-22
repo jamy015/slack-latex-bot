1dff465a307517154c1106b744791715a320fc51
Jamy Mahabier <jamysanjay@gmail.com>
2017-10-22 20:18:23 +0200

    urlencode workaround for Python 3.4

--- quicklatex.py
+++ quicklatex.py
@@ -1,8 +1,24 @@
-from urllib.parse import urlencode, quote
+from urllib.parse import quote, quote_plus
 import requests
 
 with open('default_preamble.txt') as default_preamble_file:
     default_preamble = default_preamble_file.read()
+
+
+def urlencode(query, quote_via=quote_plus):
+    """Encode a dict into a URL query string
+
+    This is a homebrew implementation of the urllib.parse.urlencode function. It's necessary because the quote_via
+    parameter of urlencode(), which is needed to prevent spaces being encoded as '+' signs (see
+    https://github.com/jamy015/slack-latex-bot/issues/4), only showed up in Python 3.5. Because Elastic Beanstalk is
+    still at Python 3.4, we have to use this ugly workaround :(
+
+    Once we can run in a Python 3.5 (or higher) environment (i.e. once Elastic Beanstalk updates to Python 3.5), we'll
+    be able to get rid of this and use the real urlencode().
+
+    This implementation is based on https://stackoverflow.com/a/42460165/
+    """
+    return '&'.join(['{}={}'.format(quote_via(k), quote_via(v)) for k, v in query.items()])
 
 
 def quicklatex(formula, preamble=default_preamble, show_errors=False):
